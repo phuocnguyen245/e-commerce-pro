462d776ba2d16fe1b3f0c76048f3c3211b58123e
nguyen truong <nguyentp@bestpicktechnology.com>
2023-09-14 18:04:19 +0700

    continue authen, register, login

--- src/routes/shop.router.ts
+++ src/routes/shop.router.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { apiKey, permission } from "../authUtils/checkAuth.ts";
 import shopController from "../controllers/shop.controller.ts";
 import { asyncHandler } from "../utils/helpers/asyncHandler.ts";
+import { verifyToken } from "../authUtils/authUtils.ts";
 const router = express.Router();
 
 router.post(
@@ -17,4 +18,9 @@ router.post(
   permission("0000") as any,
   asyncHandler(shopController.signIn)
 );
+
+// authen 
+router.use(verifyToken)
+
+
 export default router;
